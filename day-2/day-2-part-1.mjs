43a16c8cb9b874fb0b2a5cbad8ac2fa43bd0fe38
Jordan McNamara <jordan.mcnamara@gmail.com>
2023-12-04 13:18:56 +0000

    day 4

--- day-2/day-2-part-1.mjs
+++ day-2/day-2-part-1.mjs
@@ -1,15 +1,15 @@
 // @ts-nocheck
 import {readFileSync} from 'fs'
-let path = new URL("./input.txt", import.meta.url);
-let buffer = readFileSync(path);
-let input = buffer.toString().split(/\n/)
+const path = new URL("./input.txt", import.meta.url);
+const buffer = readFileSync(path);
+const input = buffer.toString().split(/\n/)
 .map(g => g.split(":"))
 .map(([game, rounds]) => [game, rounds.split(";")])
 const rules = {
     red: 12, green: 13, blue:14
 }
 
-let x = input.map(recordRound).filter(([id, isPassing])=> isPassing).reduce((acc,[id])=> acc +=id, 0)
+const x = input.map(recordRound).filter(([id, isPassing])=> isPassing).reduce((acc,[id])=> acc +=id, 0)
 console.log(x)
 
 // console.log(checkIsRoundPossible(input[0][1]))
--- day-4/day-4-part-1.mjs
+++
@@ -1,5 +0,0 @@
-// @ts-nocheck
-import { readFileSync } from "fs";
-let path = new URL("./input.txt", import.meta.url);
-let buffer = readFileSync(path);
-let input = buffer.toString().split(/\n/);
